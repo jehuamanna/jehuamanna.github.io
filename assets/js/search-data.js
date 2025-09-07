90bd657f943350868f0235e6bf5baa9881943fcb
jehuamanna <jehuamanna@gmail.com>
2025-09-07 17:47:12 +0000

    Deploying to gh-pages from @ jehuamanna/jehuamanna.github.io@faa6dccedb77e34fcf413317981c2d3a0189d40f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-github-repositories",
+          title: "Github Repositories",
+          description: "Life in Github",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
